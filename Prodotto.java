123d326aa33061d4fb28c4156bdbbeaba4f2aa4e
mattiapozzi8 <77198260+mattiapozzi8@users.noreply.github.com>
2021-01-20 16:59:43 +0100

    Add files via upload
    Ho aggiornato la classe prodotto con l'attributo NomeProdotto e ho creato la classe ProdottoDAO dove ho iniziato a fare una connessione con il database per quanto riguarda i frutti.

---
+++ Prodotto.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Prodotto {
+	
+	private String NomeProdotto;
+	private int Disponibilità;
+	private String Marca;
+	private String CodiceID;
+	private double PrezzoAlKilo;
+	private double Quantità;
+	private Date DataDiScadenza;
+	
+	public ArrayList<Carrello> IlCarrello= new ArrayList<Carrello>();
+	
+	ProdottoDAO prodDAO = new ProdottoDAO();
+
+	public int getDisponibilità() {
+		return Disponibilità;
+	}
+	public void setDisponibilità(int disponibilità) {
+		Disponibilità = disponibilità;
+	}
+	public String getMarca() {
+		return Marca;
+	}
+	public void setMarca(String marca) {
+		Marca = marca;
+	}
+	public String getCodiceID() {
+		return CodiceID;
+	}
+	public void setCodiceID(String codiceID) {
+		CodiceID = codiceID;
+	}
+	public double getPrezzoAlKilo() {
+		return PrezzoAlKilo;
+	}
+	public void setPrezzoAlKilo(double prezzoAlKilo) {
+		PrezzoAlKilo = prezzoAlKilo;
+	}
+	public double getQuantità() {
+		return Quantità;
+	}
+	public void setQuantità(double quantità) {
+		Quantità = quantità;
+	}
+	
+	public String getNomeProdotto() {
+		return NomeProdotto;
+	}
+	public void setNomeProdotto(String nomeProdotto) {
+		NomeProdotto = nomeProdotto;
+	}
+	public Date getDataDiScadenza() {
+		return DataDiScadenza;
+	}
+	public void setDataDiScadenza(Date dataDiScadenza) {
+		DataDiScadenza = dataDiScadenza;
+	}
+	
+	public void CalcolaPrezzo(){
+	}
+	
+	public Prodotto() {
+		
+	}
+	
+
+}
