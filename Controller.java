ae084b14df5a0db8ea5c6ed719136902be902d7b
zAlep99 <77438960+zAlep99@users.noreply.github.com>
2021-01-20 14:06:14 +0100

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,42 +1,44 @@
-import javax.swing.JDialog;
 
 public class Controller {
 	
 
 	FinestraDisponibilit‡ Disp;
 	SimulazioneDatabase Sim;
-	NuovaDisponibilit‡_Jdialog NewDispJdialog;
 	public int NuovaDisponibilit‡;
+	public int PrezzoTotale;
 	
 	
 	
 	public static void main(String[] args) {
 		Controller c = new Controller();
 		
-		
 	}
 	
 	public Controller() {
 		Disp=new FinestraDisponibilit‡(this);
+		Sim= new SimulazioneDatabase(this);
 		Disp.setVisible(true);
 	}
 	
-	public void ApriJDialog() {
-		NewDispJdialog=new NuovaDisponibilit‡_Jdialog(this);
-		NewDispJdialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		NewDispJdialog.setVisible(true);
-	}
-
-//	public void StampaNuovaDisp() {
-//		System.out.println(""+NuovaDisponibilit‡);
-//	}
-	public void SottraiQuantit‡() {
-//		System.out.println(""+Sim.getDisponibilit‡InMagazzino());
-		NuovaDisponibilit‡=(Sim.getDisponibilit‡InMagazzino())-(Disp.getQuantit‡DaSottrarreInt());
+	public String CalcolaPrezzo() {
+		PrezzoTotale=((Disp.getPrezzoAlKiloInt())*(Disp.getQuantit‡DaSottrarreInt()));
+		return ""+PrezzoTotale+"Ä";
 	}
 	
-	@Override
-	public String toString() {
-		return "NuovaDisponibilit‡=" + NuovaDisponibilit‡;
+	public String SottraiQuantit‡() {
+		if(Disp.getQuantit‡DaSottrarreInt()>Sim.getDisponibilit‡InMagazzino()) {
+			return"Non ci sono abbastanza scorte";
+		}
+		else {	
+		NuovaDisponibilit‡=((Sim.getDisponibilit‡InMagazzino())-(Disp.getQuantit‡DaSottrarreInt()));
+		Disp.setQuantit‡DaSottrarreInt(0);
+		if(NuovaDisponibilit‡>0) {
+		Sim.setDisponibilit‡InMagazzino(NuovaDisponibilit‡);
+		return ""+NuovaDisponibilit‡;
+		}
+		else {
+			return"Abbiamo esaurito le scorte in magazzino";	
+		}	
+		}
 	}
 }
